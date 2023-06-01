a3e238c77a5f8c01841c798d97ae35c2f3266d7a
Aleksandr Khubaev <aleksandrkhubaev04@gmail.com>
2023-06-01 17:35:53 +0300

    pog

--- django_rest1/django_rest1/asgi.py
+++ django_rest1/django_rest1/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_reat1 project.
+ASGI config for django_rest1 project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_reat1.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_rest1.settings')
 
 application = get_asgi_application()
--- django_rest1/django_rest1/urls.py
+++ django_rest1/django_rest1/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for django_reat1 project.
+URL configuration for django_rest1 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/

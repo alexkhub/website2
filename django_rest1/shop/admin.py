a67cb3bcc0118b9abec98b025ce51f524ef9a898
Aleksandr Khubaev <aleksandrkhubaev04@gmail.com>
2023-06-06 17:15:11 +0300

    pog

--- django_rest1/shop/admin.py
+++ django_rest1/shop/admin.py
@@ -1,3 +1,94 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import *
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'last_login', 'username', 'email', 'gender', 'phone', 'is_staff')
+    list_display_links = ('id', 'last_login', 'username', 'email', 'phone',)
+    prepopulated_fields = {"slug": ("username",)}
+    search_fields = ('id', 'username', 'phone', 'email')
+    list_filter = ('is_staff',)
+    list_editable = ('is_staff',)
+
+
+class TransactionsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'sum', 'transaction_date')
+    list_display_links = ('id', 'user')
+    search_fields = ('user', 'transaction_date')
+    prepopulated_fields = {"slug": ('user', 'sum', 'transaction_date')}
+
+
+
+
+class ManufacturerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'manufacturer_name', 'country')
+    list_display_links = ('manufacturer_name',)
+    search_fields = ('manufacturer_name', 'country')
+    list_filter = ('country',)
+    prepopulated_fields = {"slug": ("manufacturer_name",)}
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'manufacturer')
+    list_display_links = ('name',)
+    search_fields = ('name', 'manufacturer')
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ('name', 'manufacturer')
+
+
+class Discount_For_Product_CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category_name', 'discount_percentage', 'discount_start_date', 'discount_end_date')
+    list_display_links = ('id',)
+    search_fields = ('category_name', 'discount_start_date', 'discount_end_date')
+    list_filter = ('category_name', 'discount_start_date', 'discount_end_date')
+    prepopulated_fields = {'slug': ('category_name', 'discount_start_date', 'discount_end_date')}
+    list_editable = ('discount_end_date',)
+
+
+class Product_ImagesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'img_name', 'get_image')
+    readonly_fields = ('get_image',)
+    list_display_links = ('img_name',)
+    prepopulated_fields = {'slug': ('img_name',)}
+    search_fields = ('img_name',)
+
+    def get_image(self, obj):
+        return mark_safe(f'<img src={obj.img.url} width="60" height="60"')
+
+    get_image.short_description = "Изображение продукта"
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product_name', 'first_price', 'last_price', 'numbers', 'discount', 'category')
+    list_display_links = ('id', 'product_name')
+    search_fields = ('product_name',)
+    list_filter = ('first_price', 'last_price', 'numbers', 'discount', 'category')
+    list_editable = ('first_price', 'last_price', 'numbers', 'discount')
+    prepopulated_fields = {'slug': ('product_name',)}
+
+
+class PromotionsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'promotion_name', 'price', 'products', 'discount_start_date', 'discount_end_date')
+    list_display_links = ('id', 'promotion_name')
+    search_fields = ('promotion_name',)
+    list_filter = ('price', 'discount_start_date', 'discount_end_date')
+    list_editable = ('price',)
+    prepopulated_fields = {'slug': ('promotion_name',)}
+
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'product', 'text', 'rating')
+    list_filter = ('rating', 'product')
+    search_fields = ('user', 'product')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Transactions, TransactionsAdmin)
+admin.site.register(Manufacturer, ManufacturerAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Discount_For_Product_Category, Discount_For_Product_CategoryAdmin)
+admin.site.register(Product_Images, Product_ImagesAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Comments, CommentsAdmin)
--- django_rest1/shop/models.py
+++ django_rest1/shop/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.urls import reverse
+# from django.urls import reverse
 from django.contrib.auth.models import AbstractUser
-from django.utils.text import slugify
+from sortedm2m.fields import SortedManyToManyField
 
 
 class User(AbstractUser):
@@ -11,10 +11,143 @@ class User(AbstractUser):
     )
     gender = models.CharField(max_length=1, verbose_name="Пол", choices=Gender, blank=True)
     phone = models.CharField(max_length=20, verbose_name="Телефон", unique=True, blank=True)
-    slug = models.SlugField(max_length=50, unique=True, db_index=True, verbose_name='URL', blank=True)
+    slug = models.SlugField(max_length=50, unique=True, db_index=True, verbose_name='URL', )
     birthday = models.DateTimeField(verbose_name='Дата рождения', blank=True, null=True)
     avatar = models.ImageField(upload_to='avatars/%Y/%m/%d/', verbose_name='Аватарки', blank=True)
     description = models.TextField(verbose_name="О себе", blank=True)
 
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
     def __str__(self):
         return self.username
+
+
+class Transactions(models.Model):
+    user = models.ForeignKey('User', on_delete=models.PROTECT, verbose_name="Пользователь", blank=True)
+    sum = models.IntegerField(verbose_name='Сумма', default=0)
+    transaction_date = models.DateTimeField(verbose_name='Начало скидок', auto_now_add=True)
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='URL', )
+
+    class Meta:
+        verbose_name = 'Транзакция'
+        verbose_name_plural = 'Транзакции'
+
+
+class Manufacturer(models.Model):
+    manufacturer_name = models.CharField(max_length=100, verbose_name="Наименование производителя", unique=True)
+    country = models.CharField(max_length=40, verbose_name='Страна', blank=True)
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='URL', )
+
+    def __str__(self):
+        return self.manufacturer_name
+
+    class Meta:
+        verbose_name = 'Производитель'
+        verbose_name_plural = 'Производители'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True, verbose_name="Название категории", unique=True)
+    description = models.TextField(verbose_name="Описание", blank=True)
+    manufacturer = models.ForeignKey('Manufacturer', on_delete=models.PROTECT, null=True, verbose_name='Производитель')
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='URL', )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория продукта'
+        verbose_name_plural = 'Категории продуктов'
+
+
+class Discount_For_Product_Category(models.Model):
+    category_name = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория товаров',
+                                      blank=True)
+    discount_percentage = models.FloatField(verbose_name='Скидка')
+    discount_start_date = models.DateField(verbose_name='Начало скидок', auto_now_add=True)
+    discount_end_date = models.DateField(verbose_name='Конец скидок')
+    slug = models.SlugField(max_length=100, unique=True, db_index=True, verbose_name='URL', )
+
+    class Meta:
+        verbose_name = 'Скидка на категорию продукта'
+        verbose_name_plural = 'Скидки на категории продуктов'
+
+
+class Product_Images(models.Model):
+    img_name = models.CharField(max_length=50, verbose_name='Картинка изображения', blank=True)
+    img = models.ImageField(upload_to='img_product/%Y/%m/%d/', verbose_name='Изображение продукта')
+    slug = models.SlugField(max_length=50, unique=True, db_index=True, verbose_name='URL')
+
+    def __str__(self):
+        return self.img_name
+
+    class Meta:
+        verbose_name = 'Фотография продукта'
+        verbose_name_plural = 'Фотографии продуктов'
+
+
+class Product(models.Model):
+    product_name = models.CharField(max_length=70, verbose_name='Название продукта', unique=True, blank=True,
+                                    db_index=True)
+    first_price = models.FloatField(verbose_name='Первоначальная цена')
+    discount = models.FloatField(verbose_name='Скидка', default=0, blank=True)
+    last_price = models.FloatField(verbose_name='Конечная  цена', blank=True)
+    numbers = models.IntegerField(verbose_name='Количество продуктов')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True, verbose_name='Категория')
+    slug = models.SlugField(max_length=70, unique=True, db_index=True, verbose_name='URL', )
+    description = models.TextField(verbose_name="О продукте", blank=True)
+    product_photos = SortedManyToManyField(Product_Images, verbose_name='Изображения')
+
+    def __str__(self):
+        return self.product_name
+
+    def save(self, *args, **kwargs):
+        discount = self.discount
+        first_price = self.first_price
+        if discount > 0:
+            self.last_price = first_price * (1 - discount / 100)
+        else:
+            self.last_price = first_price
+
+        super(Product, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукция'
+        ordering = ['product_name']
+
+
+class Comments(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE, verbose_name="Пользователь")
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, verbose_name="Продукты")
+    text = models.TextField(verbose_name="Комментарий", blank=True)
+    rating = models.FloatField(verbose_name='Оценка от 1 до 10 ')
+    date = models.DateTimeField(verbose_name='Время', auto_now_add=True)
+
+
+    class Meta:
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+        ordering = ['rating']
+
+
+class Promotions(models.Model):
+    promotions_name = models.CharField(max_length=70, verbose_name='Название акции', unique=True, blank=True,
+                                       db_index=True)
+    price = models.FloatField(verbose_name='Цена акции')
+    products = SortedManyToManyField(Product, verbose_name='Продукты')
+    discount_start_date = models.DateField(verbose_name='Начало скидок', auto_now_add=True)
+    discount_end_date = models.DateField(verbose_name='Конец скидок')
+    slug = models.SlugField(max_length=70, unique=True, db_index=True, verbose_name='URL', )
+    description = models.TextField(verbose_name='Описание')
+    photo = models.ImageField(upload_to='img_product/%Y/%m/%d/', verbose_name='Картинка акции')
+
+    def __str__(self):
+        return self.promotions_name
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукция'
+        ordering = ['promotions_name']

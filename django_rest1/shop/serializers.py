76e3697552056b0446651895f328121d976f9c59
Aleksandr Khubaev <aleksandrkhubaev04@gmail.com>
2023-06-14 14:58:11 +0300

    pog

---
+++ django_rest1/shop/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+
+from .models import *
+
+class ProductsListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Products
+        field = ('product_name', 'first_price', 'discount', 'last_price')

bbe195160a8bee11803c741300839377ea50e32f
alexkhub <akeksandrkhubaev04@gmail.com>
2023-08-17 17:41:28 +0300

    pog

--- django_rest1/additional_admin_features/admin.py
+++ django_rest1/additional_admin_features/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class Products_Sales_AnalysisAdmin(admin.ModelAdmin):
+    list_display = ('id', 'product', 'date', 'amount', 'earnings')
+    list_display_links = ('id', 'product',)
+
+    search_fields = ('id', 'product', 'date')
+    list_filter = ('product', 'date', 'amount', 'earnings')
+
+class Categorys_Sales_AnalysisAdmin(admin.ModelAdmin):
+    list_display = ('id', 'category', 'date', 'amount', 'earnings')
+    list_display_links = ('id', 'category',)
+
+    search_fields = ('id', 'category', 'date')
+    list_filter = ('category', 'date', 'amount', 'earnings')
+
+admin.site.register(Products_Sales_Analysis, Products_Sales_AnalysisAdmin)
+admin.site.register(Categorys_Sales_Analysis, Categorys_Sales_AnalysisAdmin)
--- django_rest1/additional_admin_features/models.py
+++ django_rest1/additional_admin_features/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+from shop.models import Products , Category
 # Create your models here.
+
+class Products_Sales_Analysis(models.Model):
+    product = models.ForeignKey(Products , on_delete=models.PROTECT, verbose_name='Название продукта')
+    amount = models.IntegerField(default=0, verbose_name='Количество проданнах товаров')
+    earnings = models.IntegerField(default=0, verbose_name='Прибыль')
+    date = models.DateField(auto_now_add=True, verbose_name='Дата')
+
+    def __str__(self):
+        return f'Продукт-{self.product} {self.date}'
+
+    class Meta:
+        verbose_name = "Анализ продажи продукта "
+        verbose_name_plural = "Анализ продаж продуктов"
+
+
+class Categorys_Sales_Analysis(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Название продукта')
+    amount = models.IntegerField(default=0, verbose_name='Количество проданнах товаров')
+    earnings = models.IntegerField(default=0, verbose_name='Прибыль')
+    date = models.DateField(auto_now_add=True, verbose_name='Дата')
+
+    def __str__(self):
+        return f'Категория-{self.category} {self.date}'
+
+    class Meta:
+        verbose_name = "Анализ категории "
+        verbose_name_plural = "Анализ категорий"
